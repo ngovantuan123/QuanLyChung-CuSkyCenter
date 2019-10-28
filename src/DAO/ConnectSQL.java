f64a748e5af07cba8ed69f7fe25da14a1b0c5198
ngovantuan123 <ngovantuan852@gmail.com>
2019-10-28 23:24:41 +0700

    file connect

---
+++ src/DAO/ConnectSQL.java
@@ -0,0 +1,40 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class ConnectSQL {
+
+    public ConnectSQL() {
+    }
+    
+    private final static String URL = "jdbc:sqlserver://;databaseName=QuanLyChungCu";
+    private final static String USERNAME = "sa";
+    private final static String PASSWORD = "123";
+    private static Connection conn = null;
+    
+    public static Connection connect(){
+        try {
+            conn = DriverManager.getConnection(URL,USERNAME, PASSWORD);          
+                //System.out.println("co");
+        } catch (SQLException ex) {
+            //System.out.println("k");
+            Logger.getLogger(ConnectSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+    
+    public static void close(){
+        if(conn!=null) try {
+            conn.close();
+            conn = null;
+        } catch (SQLException ex) {
+            
+            Logger.getLogger(ConnectSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+    
+    
+}
